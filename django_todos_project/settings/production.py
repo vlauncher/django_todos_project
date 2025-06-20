6805401db2e0d43fbdb1b327177f9952a9fc272f
vlauncher <v2dent@gmail.com>
2025-06-20 22:36:57 +0100

    emails

--- django_todos_project/settings/production.py
+++ django_todos_project/settings/production.py
@@ -8,3 +8,12 @@ ALLOWED_HOSTS = ["*"]
 DATABASES = {
     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
+
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'v2django@gmail.com'
+EMAIL_HOST_PASSWORD = 'jmotxzuqgqxlkmlj'
